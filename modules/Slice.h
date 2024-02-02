955a1a5414bcbf8d928411c37c2f7eb876919448
ArborealAudio <arborealaudio@protonmail.com>
2024-02-02 06:20:36 -0500

    Forgot to add file

---
+++ modules/Slice.h
@@ -0,0 +1,14 @@
+// Slice.h
+
+#pragma once
+#include <stddef.h>
+
+// A modern type of data buffer.
+// The slice does not own the memory, so it must outlive the slice
+struct Slice {
+	void *data;
+	size_t len;
+};
+
+// T = data, S = size
+#define to_slice(T, S) Slice{T, S}
