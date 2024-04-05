8cc848f6797b57c14c16b10d15b21607210b3cca
Arboreal Audio <arborealaudio@protonmail.com>
2024-04-05 11:04:37 -0400

    Use templates for slice

--- modules/Slice.h
+++ modules/Slice.h
@@ -4,11 +4,13 @@
 #include <stddef.h>
 
 // A modern type of data buffer.
-// The slice does not own the memory, so it must outlive the slice
-struct Slice {
-	void *data;
-	size_t len;
-};
 
-// T = data, S = size
-#define to_slice(T, S) Slice{(T), (S)}
+template <typename T>
+struct Slice {
+	T *data;
+	size_t len;
+
+	Slice() = default;
+
+	Slice(T type[], size_t size) : data(type), len(size) {}
+};
